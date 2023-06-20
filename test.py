13b339c69d072b3366abc63a249032b20b1dd464
shailx383 <shailx383@gmail.com>
2023-06-20 17:24:42 +0530

    cleaned repo

--- test.py
+++
@@ -1,9 +0,0 @@
-def lag_plot(timeseries, lag):
-    x = timeseries.values[:-lag].tolist()
-    y = timeseries.values[lag:].tolist()
-    data = []
-    for i in range(len(x)):
-        data.append([x[i], y[i]])
-    return data
-    
-    
